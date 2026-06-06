f01e74009b87752ca13e17a05c6fe6e26f4b92d5
Satoshi Tanaka <st34.satoshi@gmail.com>
2026-06-06 20:12:04 +0900

    sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.sentry_dsn if Rails.env.production?
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.traces_sample_rate = 1.0
+  config.send_default_pii = true
+end
