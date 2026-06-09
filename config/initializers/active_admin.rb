316356f8e12fa36a60637ac009b8b1a27d5d7f82
Satoshi Tanaka <st34.satoshi@gmail.com>
2026-06-10 00:41:35 +0900

    change admin path

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -50,7 +50,7 @@ ActiveAdmin.setup do |config|
   #   config.default_namespace = false
   #
   # Default:
-  # config.default_namespace = :admin
+  config.default_namespace = Rails.application.credentials.dig(:active_admin_path)&.to_sym || :admin
   #
   # You can customize the settings for each namespace by using
   # a namespace block. For example, to change the site title
