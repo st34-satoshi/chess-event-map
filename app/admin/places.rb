a10f963dcb5fc7969f32fffff21201a21ce4101f
Satoshi Tanaka <st34.satoshi@gmail.com>
2026-06-10 00:36:09 +0900

    fix public uid

--- app/admin/places.rb
+++ app/admin/places.rb
@@ -1,18 +1,9 @@
 ActiveAdmin.register Place do
+  permit_params :name, :address, :latitude, :longitude
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :address, :latitude, :longitude, :public_uid
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :address, :latitude, :longitude, :public_uid]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  controller do
+    def find_resource
+      scoped_collection.find_by!(public_uid: params[:id])
+    end
+  end
 end
