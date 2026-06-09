ac3a23316296049beebe5950ed3f260db59e68f3
Satoshi Tanaka <st34.satoshi@gmail.com>
2026-06-10 00:56:39 +0900

    fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link application.css
 //= link application.js
 //= link active_admin.scss
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,4 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js maplibre-gl.css]
+Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js]
